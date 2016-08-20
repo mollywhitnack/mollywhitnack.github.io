ebf4c12cb95c188469043fd4a2f5bb9be14516b3
mollywhitnack <mollywhitnack@gmail.com>
2016-08-20 11:59:54 -0700

    projects

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -13,7 +13,43 @@ $(function() {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 500, 'easeInOutExpo');
+        }, 650, 'easeInOutExpo');
         event.preventDefault();
     });
 });
+
+//IE doesn't transform-style:preserve-3d.
+// for IE try http://codepen.io/rhernando/pen/vjGxH
+
+
+//using TweenLite.set() takes care of all vendor-prefixes
+TweenLite.set(".cardWrapper", {perspective:800});
+TweenLite.set(".card", {transformStyle:"preserve-3d"});
+TweenLite.set(".back", {rotationY:-180});
+TweenLite.set([".back", ".front"], {backfaceVisibility:"hidden"});
+
+$(".cardWrapper").hover(
+  function() {
+    console.log('hover');
+    TweenLite.to($(this).find(".card"), 1.2, {rotationY:180, ease:Back.easeOut});
+  },
+  function() {
+        console.log('hover');
+
+    TweenLite.to($(this).find(".card"), 1.2, {rotationY:0, ease:Back.easeOut});  
+  }
+);
+
+//a nice little intro;)
+TweenMax.staggerTo($(".card"), 1, {rotationY:-180, repeat:1, yoyo:true}, 0.1);
+
+/* learn more about GSAP JS:
+http://www.greensock.com/gsap-js/
+
+more codepen samples:
+http://codepen.io/collection/jmHAn
+
+basics:
+http://codepen.io/collection/ifybJ
+
+*/
